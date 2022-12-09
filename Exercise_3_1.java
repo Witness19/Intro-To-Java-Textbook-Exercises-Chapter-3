2428a5baffb18c7eb22aa960ff9da33687f7d39d
Elizabeth Watson <113231570+Witness19@users.noreply.github.com>
2022-12-09 11:40:25 -0500

    Exercise 3.1
    (Algebra: solve quadratic equations):
    Write a program that prompts the user to enter values for a, b, and c and displays
    the result based on the discriminant. If the discriminant is positive, display two
    roots. If the discriminant is 0, display one root. Otherwise, display “The equation
    has no real roots”.

---
+++ Exercise_3_1.java
@@ -0,0 +1,39 @@
+/*
+(Algebra: solve quadratic equations):
+Write a program that prompts the user to enter values for a, b, and c and displays
+the result based on the discriminant. If the discriminant is positive, display two
+roots. If the discriminant is 0, display one root. Otherwise, display “The equation
+has no real roots”.
+
+Date: 12/9/22
+ */
+
+import java.util.*;  // Import java.util package
+
+public class Exercise_3_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Create Scanner Object ("input")
+
+        // Prompt Input: a,b,c
+        System.out.print("Enter values for 'a', 'b', and 'c': ");
+            double a = input.nextDouble(); // Read Input: a
+            double b = input.nextDouble(); // Read Input: b
+            double c = input.nextDouble(); // Read Input: c
+        
+            double discriminant = Math.pow(b, 2) - (4 * a * c);
+            double rootOne = ((-1 * b) + Math.sqrt(discriminant)) / (2 * a);
+            double rootTwo = ((-1 * b) - Math.sqrt(discriminant)) / (2 * a);
+
+        // When the discriminant is positive, there will be two roots:
+        if (discriminant >= 1) {  // If the discriminant is greater than or equal to one, then output both roots.
+            System.out.print("The equation has two roots: " + "r1: " + rootOne + " & r2: " + rootTwo);
+        }
+        if (discriminant == 0){ // && = Output the results if both statements are true.
+            System.out.print("The equation has one root: " + rootOne);
+        }
+        if (discriminant <= -1) {
+            System.out.print("The equation has no real roots");
+        }
+        input.close();   
+    }
+}
